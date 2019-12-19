c51ffaed772ac167a95aec6b156d1457ea4f984a
BriniM <maherbrini00@gmail.com>
2019-12-19 18:56:31 +0100

    add main page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './scss/_app.scss';
+import img from './face.png';
+
+/* Icons */
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faGithubSquare, faCodepen } from "@fortawesome/free-brands-svg-icons";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<main className="main">
+			<section className="content">
+				<img src={img} id="face" alt="Maher Brini" />
+				<h1>Maher Brini</h1>
+				<h2>Business Intelligence Student</h2>
+				<p>University of Carthage, Tunis' Advanced Business Studies School</p>
+				<p>Web Full Stack & Software developer, graphic designer, motorsports fan and hiker.</p>
+				<p>New website in the works!</p>
+				{/* Icons here */}
+				<FontAwesomeIcon icon={faCodepen} />
+				<FontAwesomeIcon icon={faGithubSquare} />
+				<FontAwesomeIcon icon={faEnvelope} />
+			</section>
+		</main>
+	);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'typeface-roboto';
+import './scss/_index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
