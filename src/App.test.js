44dc6b006de47073d6c320972adddbb1144ec3fd
BriniM <maherbrini00@gmail.com>
2020-04-26 08:57:10 +0100

    Correct the App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the main component\'s header', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = getByText(/maher brini/i);
+  expect(headerElement).toBeInTheDocument();
 });
