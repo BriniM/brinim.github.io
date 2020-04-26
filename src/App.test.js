910e26b50efac209256efc91c699dc634e421ba8
BriniM <maherbrini00@gmail.com>
2020-04-26 09:23:01 +0100

    Swap branches

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders the main component\'s header', () => {
-  const { getByText } = render(<App />);
-  const headerElement = getByText(/maher brini/i);
-  expect(headerElement).toBeInTheDocument();
-});
