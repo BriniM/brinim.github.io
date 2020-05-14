4f162bbd6c3b162b6f608143d72d1c5909ab8442
BriniM <maherbrini00@gmail.com>
2020-05-14 17:51:20 +0100

    Add view1 non-responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import { Hero } from './Hero';
+
+import logoImgSrc from './Logo.png';
+import gfxImgSrc from './GFX.png';
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
+		<div>
+			{ /* FEATURE: Costumize menu for regular visitors and Club Members 
+			*    Option A: Setup an API and make this a class component
+				 Option B: Render the App by the server
+				 TODO: https://www.npmjs.com/package/react-particles-js
+			*/ }
+			<Hero  
+				header={ { logo: logoImgSrc, logoAlt: 'Logo du Club IHEC Big Data Science', 
+					menu: [ { menuItem: 'Acceuil', link: '#' },
+						{ menuItem: 'A propos de nous', link: '#' }, 
+						{ menuItem: 'Gallery', link: '#' },
+						{ menuItem: 'Contact', link: '#' },
+						{ menuItem: 'News', link: '#' },]} }
+				btn='Découvrez le Club!'
+				heading={'Premier Club Scientifique a l\'IHEC Carthage.'}
+				paragraph={'IHEC BigData Science est un club scientifique dont l’activité principale est de développer ' +
+				'la capacité professionnelle des étudiants en leurs permettant d’apprendre et de créer des réseaux dans ' +
+				'le domaine du Big Data et la science des données.' }
+				img={gfxImgSrc}
+				imgAlt="IT Infrastructure graphics"
+			/>
 		</div>
 	);
 }
---
+++ src/Hero.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import './Hero.scss';
+
+interface MenuItem {
+	menuItem: string,
+	link: string
+}
+
+interface Header {
+	logo: any,
+	logoAlt: string,
+	menu: Array<MenuItem>
+}
+
+interface HeroProps {
+	header: Header,
+	heading: string,
+	paragraph: string,
+	btn: string,
+	img: any,
+	imgAlt: string
+}
+
+export function Navigation({ logo, menu, logoAlt }: Header) {
+	return (
+		<header>
+			<nav>
+				<img src={logo} alt={logoAlt} />
+				<ul>
+					{
+						menu.map(elem => <li key={elem.menuItem}><a href={elem.link}>{elem.menuItem}</a></li>)
+					}
+				</ul>
+			</nav>
+		</header>);
+}
+
+export function Hero({ header, heading, paragraph, btn, img, imgAlt }: HeroProps) {
+	return (
+		<div className="Hero">
+			<Navigation logo={header.logo}
+				logoAlt={header.logoAlt}
+				menu={header.menu}
+			/>
+			<main>
+				<div>
+					<h1>{heading}</h1>
+					<p>{paragraph}</p>
+					<button>{btn}</button>
+				</div>
+				<img src={img} alt={imgAlt} className="responsiveImg" />
+			</main>
+		</div>);
+}
